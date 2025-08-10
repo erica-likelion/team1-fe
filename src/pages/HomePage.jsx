6dc5fef21cb59fb00391db6ff47bdc32d41da443
mna11 <thkim306@gmail.com>
2025-08-11 04:09:27 +0900

    homepage UI 작업

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,65 @@
+import ServiceCard from "@components/homepage/ServiceCard.jsx"
+import { useTranslation } from "react-i18next";
+
+// ICON은 추후 디자인 업데이트 후 맞게 변경할 예정
+import PhoneIcon from "@assets/images/logo.svg";
+import ChatIcon from "@assets/images/logo.svg";
+import MedicineIcon from "@assets/images/logo.svg";
+
+const HomePage = () => {
+    const { t } = useTranslation();
+
+    const serviceCards = [
+        {
+            icon: PhoneIcon,
+            step: t('homepage.serviceCard.preVisit.step'),
+            title: t('homepage.serviceCard.preVisit.title'),
+            description: t('homepage.serviceCard.preVisit.description'),
+            onClick: () => console.log('진료 전 클릭')
+        },
+        {
+            icon: ChatIcon,
+            step: t('homepage.serviceCard.inVisit.step'),
+            title: t('homepage.serviceCard.inVisit.title'),
+            description: t('homepage.serviceCard.inVisit.description'),
+            onClick: () => console.log('진료 중 클릭')
+        },
+        {
+            icon: MedicineIcon,
+            step: t('homepage.serviceCard.postVisit.step'),
+            title: t('homepage.serviceCard.postVisit.title'),
+            description: t('homepage.serviceCard.postVisit.description'),
+            onClick: () => console.log('진료 후 클릭')
+        }
+    ];
+
+    return (
+        <div className="flex flex-col items-center">
+            <div className="px-5 py-2.5 self-start">
+                <p className="font-semibold">
+                    {t('homepage.headerPart1')}<br />
+                    <span className="text-[#3DE0AB]">{t('homepage.headerPart2')}</span>
+                    {t('homepage.headerPart3')}
+                </p>
+            </div>
+            <div className="w-[331px] border-[1px] border-[#E9E9EA]"></div>
+            <div>
+                <p className="py-10 font-semibold text-[20px]">{t('homepage.main')}</p>
+            </div>
+            <div className="flex flex-col gap-5">
+                {serviceCards.map((card, index) => (
+                    <ServiceCard
+                        key={index}
+                        icon={card.icon}
+                        step={card.step}
+                        title={card.title}
+                        description={card.description}
+                        onClick={card.onClick}
+                    />
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default HomePage;
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,9 +1,20 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import MainLayout from '@components/MainLayout';
+import HomePage from '@pages/HomePage';
+import ChatPage from '@pages/ChatPage';
+import MyPage from '@pages/MyPage';
+
 const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
+                <Route path="/" element={<MainLayout />}>
+                    <Route index element={<HomePage />} />
+                    <Route path="home" element={<HomePage />} />
+                    <Route path="chat" element={<ChatPage />} />
+                    <Route path="mypage" element={<MyPage />} />
+                </Route>
             </Routes>
         </BrowserRouter>
     )
