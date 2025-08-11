4da8f9be8557730b046c53f3be4ef199919dda3e
MNA11 <thkim306@gmail.com>
2025-08-11 16:49:19 +0900

    Refactor : 구성에 NavBar의 설정을 반환해주는 Utils 함수 제작 및 Layout에 적용

---
+++ src/utils/NavConfig.js
@@ -0,0 +1,71 @@
+// NavBar 설정 관리 파일 - 현재 페이지에 맞는 구성을 반환
+
+// NavBar 타입 결정
+const getNavBarType = (pathname) => {
+    if (pathname === '/home' || pathname === '/') return 'home';
+    if (pathname === '/mypage') return 'mypage';
+    if (pathname === '/chat') return 'chat';
+    if (pathname === '/language') return 'language';
+    return 'default';
+};
+
+// 타입에 맞는 NavBar 타이틀 (TopNavBar 중앙 텍스트)
+const getNavBarTitle = (type, t) => {
+    switch (type) {
+        case 'mypage':
+            return t('navigation.mypage');
+        case 'chat':
+            return t('navigation.chat');
+        case 'language':
+            return t('navigation.language');
+        default:
+            return "";
+    }
+};
+
+// NavBar 타입별 핸들러 생성, LeftClick RightClick 순
+const getNavBarHandlers = (type, navigate) => {
+    switch (type) {
+        case 'home':
+            return {
+                onLeftClick: () => navigate('/home'),
+                onRightClick: () => navigate('/language')
+            };
+        case 'mypage':
+            return {
+                onLeftClick: () => {
+                    // 추후 수정: 알림 기능
+                    console.log('알림 클릭');
+                },
+                onRightClick: () => navigate('/language')
+            };
+        case 'chat':
+            return {
+                onLeftClick: () => {
+                    // 추후 수정: 검색 기능
+                    console.log('검색 클릭');
+                },
+                onRightClick: () => navigate('/language')
+            };
+        case 'language':
+        case 'default':
+        default:
+            return {
+                onLeftClick: () => navigate(-1), // 뒤로가기
+                onRightClick: () => navigate('/home') // 홈으로
+            };
+    }
+};
+
+// NavBar 설정 통합 함수
+export const getNavBarConfig = (pathname, navigate, t) => {
+    const type = getNavBarType(pathname);
+    const title = getNavBarTitle(type, t);
+    const handlers = getNavBarHandlers(type, navigate);
+
+    return {
+        type,
+        title,
+        ...handlers
+    };
+};
