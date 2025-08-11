ee92396cee0fb0eee4223cbad91e81eb783f0180
DoggiKim <kmss0426@hanyang.ac.kr>
2025-08-11 22:46:43 +0900

    feat(ui): 스타일 적용(TextButton/TitleBlock/TextField) - 기본 여백·타이포 반영 - hover/disabled 상태 추가 - 로직 변경 없음

--- src/components/commons/TextButton.jsx
+++ src/components/commons/TextButton.jsx
@@ -1,12 +1,26 @@
+import Right from "@assets/images/chevron_right.svg";
+
 const TextButton = ({ text, onClick, disabled = false}) => {
     return (
      
         <button
                 onClick={onClick}
                 disabled={disabled}
-                // 입력값이 적용되지 않았을 때 버튼값 css 넣어야함
+                className={`
+                    flex flex-row justify-center items-center
+                    px-5 py-2 gap-2.5
+                    w-80 h-12
+                    rounded-xl font-medium
+                    ${disabled 
+                        ? "bg-gray-200 text-white cursor-not-allowed" 
+                        : "bg-[#3DE0AB] text-white hover:bg-[#00C88D] active:bg-[#00C88D]"
+                    }    
+                    `}
                 >
                 {text}
+                <div className="flex justify-end items-center">
+                    <img className="w-6 h-6" src={Right} />
+                </div>
         </button>
        
     );
