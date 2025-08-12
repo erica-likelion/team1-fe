f26aeb82248b186296c8ecc9b3d02ac39a8241c4
MNA11 <thkim306@gmail.com>
2025-08-12 21:20:52 +0900

    Feat: TextButton icon 기능 확장 & 디자인 변경 (fixed, 추가 디자인 props)

--- src/components/commons/TextButton.jsx
+++ src/components/commons/TextButton.jsx
@@ -3,9 +3,13 @@ const TextButton = ({ text, icon, onClick, disabled = false, className=""}) => {
         <button
                 onClick={onClick}
                 disabled={disabled}
-                className={`flex items-center justify-center py-3 gap-2.5 bg-[#3DE0AB]
-                     text-[#FAFAFA] font-medium rounded-lg hover:cursor-pointer
-                     fixed bottom-5 left-1/2 transform -translate-x-1/2 max-w-[375px] w-full z-50 ${className}`}
+                className={`flex flex-row justify-center items-center 
+                            px-5 py-3 gap-2.5
+                            font-medium rounded-md
+                     ${disabled 
+                        ? "bg-gray-200 text-white cursor-not-allowed" 
+                        : "bg-[#3DE0AB] text-white hover:cursor-pointer hover:bg-[#00C88D] active:bg-[#00C88D]"}    
+                     fixed bottom-5 left-1/2 transform -translate-x-1/2 max-w-[335px] w-full z-50 ${className}`}
                 >
                 {text}
                 {icon && 
