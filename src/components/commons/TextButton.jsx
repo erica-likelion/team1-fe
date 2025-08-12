c531e6e93cb43b91f3fe828b4b5956de5f15213d
MNA11 <thkim306@gmail.com>
2025-08-12 20:44:50 +0900

    Feat: icon 변경 기능 확장

--- src/components/commons/TextButton.jsx
+++ src/components/commons/TextButton.jsx
@@ -1,14 +1,16 @@
-const TextButton = ({ text, onClick, disabled = false}) => {
+const TextButton = ({ text, icon, onClick, disabled = false, className=""}) => {
     return (
-     
         <button
                 onClick={onClick}
                 disabled={disabled}
-                // 입력값이 적용되지 않았을 때 버튼값 css 넣어야함
+                className={`flex items-center justify-center py-3 gap-2.5 bg-[#3DE0AB]
+                     text-[#FAFAFA] font-medium rounded-lg hover:cursor-pointer
+                     fixed bottom-5 left-1/2 transform -translate-x-1/2 max-w-[375px] w-full z-50 ${className}`}
                 >
                 {text}
+                {icon && 
+                <img src={icon} className="inline" />}
         </button>
-       
     );
 }
 
