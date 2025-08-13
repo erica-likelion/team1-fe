b9a44ec33440bcb7ad1f779ff70e0e8d2916792c
MNA11 <thkim306@gmail.com>
2025-08-13 22:01:35 +0900

    Feat: icon 없는 경우 확장

--- src/components/homepage/ServiceCard.jsx
+++ src/components/homepage/ServiceCard.jsx
@@ -11,12 +11,13 @@ const ServiceCard = ({
     
   return (
       <div 
-      className={`flex items-center w-full p-3 pr-2 gap-5 rounded-sm cursor-pointer shadow-[0_0_12px_0_rgba(23,23,27,0.15)] ${className}`}
+      className={`flex items-center w-full py-3 pr-2 gap-5 rounded-sm cursor-pointer shadow-[0_0_12px_0_rgba(23,23,27,0.15)] ${icon ? "pl-3" : "pl-4.5" } ${className}`}
       onClick={onClick}
       >
+        {icon && 
         <div className="w-14 h-14 bg-[#C5F4E1] rounded-sm flex items-center justify-center overflow-hidden">
             <img src={icon} />
-        </div>
+        </div>}
         <div className="flex-1">
             <p className="font-semibold">{title}</p>
             <p className="font-normal text-[12px]">{description}</p>
