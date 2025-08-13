30ef7d9fbd710950bb3ffd4d449c380bbf6b456e
MNA11 <thkim306@gmail.com>
2025-08-13 21:49:47 +0900

    Feat: ServiceCard className props 추가

--- src/components/homepage/ServiceCard.jsx
+++ src/components/homepage/ServiceCard.jsx
@@ -6,11 +6,12 @@ const ServiceCard = ({
   title,
   description,
   onClick,
+  className=""
 }) => {
     
   return (
       <div 
-      className={"flex items-center w-full p-3 pr-2 gap-5 rounded-sm cursor-pointer shadow-[0_0_12px_0_rgba(23,23,27,0.15)]"}
+      className={`flex items-center w-full p-3 pr-2 gap-5 rounded-sm cursor-pointer shadow-[0_0_12px_0_rgba(23,23,27,0.15)] ${className}`}
       onClick={onClick}
       >
         <div className="w-14 h-14 bg-[#C5F4E1] rounded-sm flex items-center justify-center overflow-hidden">
