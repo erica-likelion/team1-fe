f4193c22051c091d3c40f50acc7a264833227d4a
MNA11 <thkim306@gmail.com>
2025-08-14 15:46:41 +0900

    feat: splashPage 구현 후, 라우팅 구현

---
+++ src/pages/SplashPage.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+import { useTranslation } from "react-i18next";
+import Logo from "@assets/images/logo.svg";
+
+const SplashPage = () => {
+    const navigate = useNavigate();
+    const { t } = useTranslation();
+    const [isVisible, setIsVisible] = useState(true);
+
+    useEffect(() => {
+        const fadeTimer = setTimeout(() => {
+            setIsVisible(false);
+        }, 1500);
+
+        const navigateTimer = setTimeout(() => {
+            navigate('/home');
+        }, 2000);
+
+        return () => {
+            clearTimeout(fadeTimer);
+            clearTimeout(navigateTimer);
+        };
+    }, [navigate]);
+
+    return (
+        <div 
+            className={`flex flex-col justify-center items-center w-screen h-screen transition-opacity duration-500 ${
+                isVisible ? 'opacity-100' : 'opacity-0'
+            }`}
+        >
+            <img src={Logo} className="w-[177px] h-[177px]"/>
+            <p className="mt-[-18px] text-[28px] font-medium text-[#3DE0AB]">{t('brand')}</p>
+        </div>
+    )
+}
+
+export default SplashPage;
