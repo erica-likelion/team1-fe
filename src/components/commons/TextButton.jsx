e1f993c07b8a5a0919d05c0b1ac4778cd1f1d58c
MNA11 <thkim306@gmail.com>
2025-08-21 20:41:37 +0900

    Feat: progress props 추가

--- src/components/commons/TextButton.jsx
+++ src/components/commons/TextButton.jsx
@@ -1,17 +1,24 @@
-const TextButton = ({ text, icon, onClick, disabled = false, className=""}) => {
+const TextButton = ({ text, progress = "", icon, onClick, disabled = false, className=""}) => {
+    const [current, total] = progress?.split('/');
+
     return (
         <button
                 onClick={onClick}
                 disabled={disabled}
                 className={`flex flex-row justify-center items-center 
                             px-5 py-3 gap-2.5
-                            font-medium rounded-md
+                            font-semibold rounded-md
                      ${disabled 
                         ? "bg-gray-200 text-white cursor-not-allowed" 
                         : "bg-[#3DE0AB] text-white hover:cursor-pointer hover:bg-[#00C88D] active:bg-[#00C88D]"}    
                      fixed bottom-5 left-1/2 transform -translate-x-1/2 max-w-[335px] w-full z-50 ${className}`}
                 >
                 {text}
+                <div className="flex gap-0.5 font-medium">
+                    <p>{current}</p>
+                    <p>{'/'}</p>
+                    <p>{total}</p>
+                </div>
                 {icon && 
                 <img src={icon} className="inline" />}
         </button>
