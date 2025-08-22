019f31d9aeb9f6da64ec6d32b36157454d958aaf
MNA11 <thkim306@gmail.com>
2025-08-22 15:24:01 +0900

    Comment: 주석 삭제

--- src/apis/callApi.js
+++ src/apis/callApi.js
@@ -6,7 +6,6 @@ import api from "@utils/apiClient";
  */
 export const getCallHistory = async () => {
     try {
-        // 현재는 임시 데이터 반환 (API 엔드포인트가 생기면 수정)
         return [];
     } catch (error) {
         console.error('통화 기록을 가져오는 중 오류 발생:', error);
