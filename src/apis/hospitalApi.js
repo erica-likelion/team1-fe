191d20dc0eb2e57a88e12ae11e457e0938c8149a
MNA11 <thkim306@gmail.com>
2025-08-24 02:12:38 +0900

    Feat: 환경변수 이용이 아닌 동적 변경

--- src/apis/hospitalApi.js
+++ src/apis/hospitalApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_LOCAL_API_BASE_URL;
+const API_BASE_URL = window.location.origin;
 
 const api = axios.create({
   baseURL: API_BASE_URL || 'http://localhost:8080/',
