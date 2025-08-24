51d8e2387e7336bf906fcbcf8db92ef22e8dc0ff
MNA11 <thkim306@gmail.com>
2025-08-24 20:52:22 +0900

    Design: figma에 맞게 디자인 수정

--- src/pages/LanguagePage.jsx
+++ src/pages/LanguagePage.jsx
@@ -15,15 +15,15 @@ const LanguagePage = () => {
 
     return (
         <div className="flex flex-col items-center pt-3 px-5 gap-1.5">
-            <div className="px-5 w-[100%]">
-                {SUPPORTED_LANGUAGES.map((language) => (
+            <div className="w-[100%]">
+                {SUPPORTED_LANGUAGES.map((language, index) => (
                     <div
                         key={language.code}
                         onClick={() => handleLanguageChange(language.code)}
-                        className="px-4.5 py-4.5 border-b-2 border-[#A6A9AA] cursor-pointer"
+                        className={`px-4.5 pb-1.5 cursor-pointer ${index !== SUPPORTED_LANGUAGES.length - 1 ? "border-b-2 border-[#A6A9AA]" : "border-none"}`}
                     >
-                        <div className="flex items-center justify-between">
-                            <span className="font-medium">{language.name}</span>
+                        <div className="px-2 pb-3 pt-4.5 flex items-center justify-between">
+                            <span className="font-normal">{language.name}</span>
                             {i18n.language === language.code ? (
                                 <img className="w-6 h-6" alt="selected_checkbox" src={ActiveCheckbox} />
                             ) : (<img className="w-6 h-6" alt="unselected_checkbox" src={Checkbox} />)}
