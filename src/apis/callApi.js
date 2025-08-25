d7c955f77d733a83d3d9408141e39a273ca83f8e
MNA11 <thkim306@gmail.com>
2025-08-25 11:55:11 +0900

    Feat: 전화 예약 더미 데이터 추가

--- src/apis/callApi.js
+++ src/apis/callApi.js
@@ -8,16 +8,16 @@ export const getCallHistory = async () => {
     try {
         return [
             {
-        id: "chat_001",
-        title: "AI 자동 생성 제목",
-        description: "내과 예약 관련 통역 채팅",
-        createdAt: "2025-08-25"
+            id: "call_001",
+            title: "안산고잔바로튼튼의원 - 예약",
+            description: "복통 및 소화불량 증상",
+            createdAt: "2025-08-25"
             },
             {
-        id: "chat_001",
-        title: "제목",
-        description: "내과 예약 관련 통역 채팅",
-        createdAt: "2025-08-25"
+            id: "call_002", 
+            title: "건강한미래항외과의원 - 예약",
+            description: "어깨 통증 증상",
+            createdAt: "2025-08-25"
             },
 
         ];
